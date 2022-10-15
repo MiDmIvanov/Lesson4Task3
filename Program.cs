b8208ea8a64c927ef58c93588d87fae7ceaef2e7
admishka <mu@tabletr.ru>
2022-10-15 14:31:46 +0300

    Задача 3

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите функцию, которая задаёт массив из 8 элементов и выводит их на экран.
+string A = "";
+Console.WriteLine("Введите массив до 8 символов через запятую:");
+A = Console.ReadLine()!;
+Array(A);
+void Array(string _A)
+{
+    string[] arr = _A.Split(',');
+    int N = arr.Length;
+    for (int i = 0; i < N; i++)
+    {
+        Console.Write($"[{arr[i]}], ");
+    }
+}
